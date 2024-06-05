b251da9d6617e2c144cd66c8fd7a49ec224b3d52
Jasonvu123 <98994631+Jasonvu123@users.noreply.github.com>
2024-06-04 21:08:53 -0400

    Made Categories Side by Side
    Removed hands
    Placed all categories side by side

--- my-nba-player/src/app/page.tsx
+++ my-nba-player/src/app/page.tsx
@@ -15,7 +15,6 @@ const IndexPage: React.FC = () => {
     postHook: 50,
     postFade: 50,
     postControl: 50,
-    hands: 50,
     interiorDefense: 50,
     perimeterDefense: 50,
     steal: 50,
@@ -70,71 +69,74 @@ const IndexPage: React.FC = () => {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1 style={{ fontSize: '2rem', textDecoration: 'underline' }}>NBA 2k24 Player Comparison</h1>
       <div>
-      <h2 style={{ maxWidth: '800px', margin: '0 auto', textAlign: 'center', marginBottom: '20px'  }}>
-        Answer each of the following prompts by dragging the slider. 
-        Afterward, you can filter the list of current players by their 2K rating. 
-        You will be matched with a player whose rating is greater than or equal to the filter value. 
-        Once matched, you can view your own radar chart and the player. Enjoy!
-      </h2>
+        <h2 style={{ maxWidth: '800px', margin: '0 auto', textAlign: 'center', marginBottom: '20px' }}>
+          Answer each of the following prompts by dragging the slider. 
+          Afterward, you can filter the list of current players by their 2K rating. 
+          You will be matched with a player whose rating is greater than or equal to the filter value. 
+          Once matched, you can view your own radar chart and the player. Enjoy!
+        </h2>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginRight: '50px' }}>
-        <div style={{ marginBottom: '10px' }}>
-        <h3 style={{fontSize: '1.2rem', textDecoration: 'underline' }}>Outside Scoring</h3>
-          {renderAttributeInputs('closeShot', customPlayer.closeShot ? customPlayer.closeShot : 50)}
-          {renderAttributeInputs('midRangeShot', customPlayer.midRangeShot ? customPlayer.midRangeShot : 50)}
-          {renderAttributeInputs('threePointShot', customPlayer.threePointShot ? customPlayer.threePointShot : 50)}
-          {renderAttributeInputs('freeThrow', customPlayer.freeThrow ? customPlayer.freeThrow : 50)}
-          {renderAttributeInputs('shotIQ', customPlayer.shotIQ ? customPlayer.shotIQ : 50)}
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', maxWidth: '1200px', margin: '0 auto' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+          <div style={{ marginRight: '20px' }}>
+            <h3 style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>Outside Scoring</h3>
+            {renderAttributeInputs('closeShot', customPlayer.closeShot ? customPlayer.closeShot : 50)}
+            {renderAttributeInputs('midRangeShot', customPlayer.midRangeShot ? customPlayer.midRangeShot : 50)}
+            {renderAttributeInputs('threePointShot', customPlayer.threePointShot ? customPlayer.threePointShot : 50)}
+            {renderAttributeInputs('freeThrow', customPlayer.freeThrow ? customPlayer.freeThrow : 50)}
+            {renderAttributeInputs('shotIQ', customPlayer.shotIQ ? customPlayer.shotIQ : 50)}
+          </div>
         </div>
-        <div style={{ marginBottom: '10px'}}>
-        <h3 style={{fontSize: '1.2rem', textDecoration: 'underline' }}>Playmaking</h3>
-          {renderAttributeInputs('passAccuracy', customPlayer.passAccuracy ? customPlayer.passAccuracy : 50)}
-          {renderAttributeInputs('ballHandle', customPlayer.ballHandle ? customPlayer.ballHandle : 50)}
-          {renderAttributeInputs('passIQ', customPlayer.passIQ ? customPlayer.passIQ : 50)}
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+          <div style={{ marginRight: '20px' }}>
+            <h3 style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>Inside Scoring</h3>
+            {renderAttributeInputs('layup', customPlayer.layup ? customPlayer.layup : 50)}
+            {renderAttributeInputs('postHook', customPlayer.postHook ? customPlayer.postHook : 50)}
+            {renderAttributeInputs('postFade', customPlayer.postFade ? customPlayer.postFade : 50)}
+            {renderAttributeInputs('postControl', customPlayer.postControl ? customPlayer.postControl : 50)}
+          </div>
         </div>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', margin: '0 20px' }}>
-        <div style={{ marginBottom: '10px' }}>
-        <h3 style={{fontSize: '1.2rem', textDecoration: 'underline' }}>Inside Scoring</h3>
-          {renderAttributeInputs('layup', customPlayer.layup ? customPlayer.layup : 50)}
-          {renderAttributeInputs('postHook', customPlayer.postHook ? customPlayer.postHook : 50)}
-          {renderAttributeInputs('postFade', customPlayer.postFade ? customPlayer.postFade : 50)}
-          {renderAttributeInputs('postControl', customPlayer.postControl ? customPlayer.postControl : 50)}
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+          <div style={{ marginRight: '20px' }}>
+            <h3 style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>Playmaking</h3>
+            {renderAttributeInputs('passAccuracy', customPlayer.passAccuracy ? customPlayer.passAccuracy : 50)}
+            {renderAttributeInputs('ballHandle', customPlayer.ballHandle ? customPlayer.ballHandle : 50)}
+            {renderAttributeInputs('passIQ', customPlayer.passIQ ? customPlayer.passIQ : 50)}
+          </div>
         </div>
-        <div style={{ marginBottom: '10px' }}>
-        <h3 style={{fontSize: '1.2rem', textDecoration: 'underline' }}>Athleticism</h3>
-          {renderAttributeInputs('speed', customPlayer.speed ? customPlayer.speed : 50)}
-          {renderAttributeInputs('strength', customPlayer.strength ? customPlayer.strength : 50)}
-          {renderAttributeInputs('hustle', customPlayer.hustle ? customPlayer.hustle : 50)}
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+          <div style={{ marginRight: '20px' }}>
+            <h3 style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>Athleticism</h3>
+            {renderAttributeInputs('speed', customPlayer.speed ? customPlayer.speed : 50)}
+            {renderAttributeInputs('strength', customPlayer.strength ? customPlayer.strength : 50)}
+            {renderAttributeInputs('hustle', customPlayer.hustle ? customPlayer.hustle : 50)}
+          </div>
         </div>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginLeft: '50px'}}>
-        <div style={{ marginBottom: '10px' }}>
-        <h3 style={{fontSize: '1.2rem', textDecoration: 'underline' }}>Defense</h3>
-          {renderAttributeInputs('hands', customPlayer.hands ? customPlayer.hands : 50)}
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
+        <div style={{ marginRight: '20px' }}>
+          <h3 style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>Rebounding</h3>
+          {renderAttributeInputs('offensiveRebound', customPlayer.offensiveRebound ? customPlayer.offensiveRebound : 50)}
+          {renderAttributeInputs('defensiveRebound', customPlayer.defensiveRebound ? customPlayer.defensiveRebound : 50)}
+        </div>
+        <div style={{ marginRight: '20px' }}>
+          <h3 style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>Defense</h3>
           {renderAttributeInputs('interiorDefense', customPlayer.interiorDefense ? customPlayer.interiorDefense : 50)}
           {renderAttributeInputs('perimeterDefense', customPlayer.perimeterDefense ? customPlayer.perimeterDefense : 50)}
           {renderAttributeInputs('steal', customPlayer.steal ? customPlayer.steal : 50)}
           {renderAttributeInputs('block', customPlayer.block ? customPlayer.block : 50)}
           {renderAttributeInputs('helpDefenseIQ', customPlayer.helpDefenseIQ ? customPlayer.helpDefenseIQ : 50)}
         </div>
-        <div style={{ marginBottom: '10px' }}>
-        <h3 style={{fontSize: '1.2rem', textDecoration: 'underline' }}>Rebounding</h3>
-          {renderAttributeInputs('offensiveRebound', customPlayer.offensiveRebound ? customPlayer.offensiveRebound : 50)}
-          {renderAttributeInputs('defensiveRebound', customPlayer.defensiveRebound ? customPlayer.defensiveRebound : 50)}
-        </div>
       </div>
       </div>
       {mostSimilarPlayer.player && (
         <div>
-          <h1 style={{fontSize: '1.2rem', textDecoration: 'underline' }}>Most Similar Player:</h1>
+          <h1 style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>Most Similar Player:</h1>
           <p>Name: {mostSimilarPlayer.player.name}</p>
           <p>Distance: {mostSimilarPlayer.distance}</p>
         </div>
       )}
     </div>
-  );
+  );  
 };  
 
 export default IndexPage;
